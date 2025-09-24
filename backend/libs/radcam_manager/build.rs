c7749b569ac9c354306b5ccb48168a24131b37d1
João Antônio Cardoso <joao.maker@gmail.com>
2025-09-24 20:48:31 -0300

    backend: libs: radcam_manager: Add build.rs that emits vergen env vars

---
+++ backend/libs/radcam_manager/build.rs
@@ -0,0 +1,24 @@
+use vergen_gix::{BuildBuilder, CargoBuilder, DependencyKind, GixBuilder};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    generate_build_details()?;
+
+    Ok(())
+}
+
+fn generate_build_details() -> Result<(), Box<dyn std::error::Error>> {
+    let mut emitter = vergen_gix::Emitter::default();
+
+    emitter.add_instructions(&BuildBuilder::all_build()?)?;
+    emitter.add_instructions(
+        CargoBuilder::all_cargo()?.set_dep_kind_filter(Some(DependencyKind::Normal)),
+    )?;
+
+    if std::path::Path::new("../../.git").is_dir() {
+        emitter.add_instructions(&GixBuilder::all_git()?)?;
+    }
+
+    emitter.emit()?;
+
+    Ok(())
+}
